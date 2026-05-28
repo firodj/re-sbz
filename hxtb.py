ff130581978880a2b2a745fabe0d08ffc1498e57
Fadhil Mandaga <1750918+firodj@users.noreply.github.com>
2026-05-28 14:41:13 +0700

    chore: add hxtb peru

---
+++ hxtb.py
@@ -0,0 +1,401 @@
+import ida_hexrays as hx
+import ida_bytes
+import idautils
+import ida_kernwin
+import ida_lines
+import ida_funcs
+import idc
+from ida_idaapi import __EA64__, BADADDR
+
+__author__ = "Dennis Elser @ https://github.com/patois"
+SCRIPT_NAME = "hxtb"
+
+"""
+Hexrays Toolbox - IDAPython plugin for finding code patterns using Hexrays
+==========================================================================
+
+This IDAPython script allows code patterns to be found within binaries whose
+processor architecture is supported by the Hexrays decompiler
+(https://www.hex-rays.com/).
+
+HRDevHelper (https://github.com/patois/HRDevHelper) is a separate IDAPython
+plugin for IDA Pro that visualizes the AST of decompiled code. Its use is
+encouraged in combination with Hexrays Toolbox, in order to simplify the
+development of queries.
+
+Use Cases:
+----------
+- scan binary files for known and unknown vulnerabilities
+- locate code patterns from previously reverse engineered executables
+  within newly decompiled code
+- malware variant analysis
+- find code similarities across several binaries
+- find code patterns from one architecture within executable code of another
+  architecture
+- many more, limited (almost) only by the queries you'll come up with ;)
+
+Example scenarios:
+------------------
+Load and run one of the accompanied scripts, such as 'example_queries.py'
+with IDA (Shift-F2).
+
+Todo:
+-----
+- data flow analysis
+- this should be optimized for speed and rewritten in C/C++ :)
+- explicit support for cinsn_t?
+"""
+
+# ----------------------------------------------------------------------------
+class query_object_t():
+    """function queries (as used by hxtb-shell) inerhit from this class
+    and should override the below methods.
+    hxtb-shell will run them in the following order:
+    1) __init__()
+    2) init()
+    3) get_scope()
+    4) run()
+    5) exit()"""
+    def __init__(self):
+        pass
+
+    def init(self):
+        """this is called after the object has been instantiated.
+        it can be used to determine whether run() should be
+        invoked or not (return True or False)"""
+        return False
+
+    def run(self, cfunc, citem):
+        """implement query here
+        return:
+           True: pattern match
+           False: pattern mismatch"""
+        return False
+
+    def exit(self):
+        """cleanup work etc..."""
+        return
+
+    def get_scope(self):
+        """return list of addresses to run the query function on (run())"""
+        return []
+
+# ----------------------------------------------------------------------------
+class query_result_t():
+    def __init__(self, cfunc=None, i=None):
+        if isinstance(cfunc, hx.cfuncptr_t):
+            self.entry = cfunc.entry_ea
+        elif isinstance(cfunc, int):
+            self.entry = cfunc
+        else:
+            self.entry = BADADDR
+        if isinstance(i, (hx.cexpr_t, hx.cinsn_t)):
+            self.ea = i.ea if not isinstance(cfunc, hx.cfuncptr_t) else self.find_closest_address(cfunc, i)
+            self.v = ida_lines.tag_remove(i.print1(None))
+        elif isinstance(i, tuple):
+            self.ea, self.v = i
+        else:
+            self.ea = BADADDR
+            self.v = "<undefined>"
+
+    def find_closest_address(self, cfunc, i):
+        parent = i
+        while parent:
+            if parent and parent.ea != BADADDR:
+                return parent.ea
+            parent = cfunc.body.find_parent_of(parent)
+        return BADADDR
+
+    def __str__(self):
+        return "[%x] %x: \"%s\"" % (self.entry, self.ea, self.v)
+
+# ----------------------------------------------------------------------------
+def find_item(ea, q, parents=False, flags=0):
+    """find item within AST of decompiled function
+
+    arguments:
+    ea:         address belonging to a function
+    q:          lambda/function: f(cfunc_t, citem_t) returning a bool
+    parents:    False -> discard cexpr_t parent nodes
+                True  -> maintain citem_t parent nodes
+
+    returns list of query_result_t objects
+    """
+
+    f = ida_funcs.get_func(ea)
+    if f:
+        cfunc = None
+        hf = hx.hexrays_failure_t()
+        try:
+            cfunc = hx.decompile(f, hf, flags)
+        except Exception as e:
+            print("[%s] %x: unable to decompile: '%s'" % (SCRIPT_NAME, ea, hf))
+            print("\t (%s)" % e)
+            return list()
+
+        if cfunc:
+            return find_child_item(cfunc, cfunc.body, q, parents)
+    return list()
+
+# ----------------------------------------------------------------------------
+def find_child_item(cfunc, i, q, parents=False):
+    """find child item in cfunc_t starting at citem_t i
+    
+    arguments:
+    cfunc:      cfunc_t
+    i:          citem_t
+    q:          lambda/function: f(cfunc_t, citem_t) returning a bool
+
+    returns list of query_result_t objects
+    """
+
+    class citem_finder_t(hx.ctree_visitor_t):
+        def __init__(self, cfunc, q, parents):
+            hx.ctree_visitor_t.__init__(self,
+                hx.CV_PARENTS if parents else hx.CV_FAST)
+
+            self.cfunc = cfunc
+            self.query = q
+            self.found = list()
+            return
+
+        def process(self, i):
+            """process cinsn_t and cexpr_t elements alike"""
+
+            try:
+                if self.query(self.cfunc, i):
+                    self.found.append(query_result_t(self.cfunc, i))
+            except:
+                pass
+            return 0
+
+        def visit_insn(self, i):
+            return self.process(i)
+
+        def visit_expr(self, e):
+            return self.process(e)
+
+    if cfunc:
+        itfinder = citem_finder_t(cfunc, q, parents)
+        itfinder.apply_to(i, None)
+        return itfinder.found
+    return list()
+
+# ----------------------------------------------------------------------------
+def find_expr(ea, q, parents=False, flags=0):
+    """find expression within AST of decompiled function
+    
+    arguments:
+    ea:         address belonging to a function
+    q:          lambda/function: f(cfunc_t, citem_t) returning a bool
+    parents:    False -> discard cexpr_t parent nodes
+                True  -> maintain citem_t parent nodes
+
+    returns list of query_result_t objects
+    """
+
+    f = ida_funcs.get_func(ea)
+    if f:
+        cfunc = None
+        hf = hx.hexrays_failure_t()
+        try:
+            cfunc = hx.decompile(f, hf, flags)
+        except Exception as e:
+            print("[%s] %x: unable to decompile: '%s'" % (SCRIPT_NAME, ea, hf))
+            print("\t (%s)" % e)
+            return list()
+
+        if cfunc:
+            return find_child_expr(cfunc, cfunc.body, q, parents)
+    return list()
+
+# ----------------------------------------------------------------------------
+def find_child_expr(cfunc, e, q, parents=False):
+    """find child expression in cfunc_t starting at cexpr_t e
+    
+    arguments:
+    cfunc:      cfunc_t
+    e:          cexpr_t
+    q:          lambda/function: f(cfunc_t, citem_t) returning a bool
+
+    returns list of query_result_t objects
+    """
+
+    class expr_finder_t(hx.ctree_visitor_t):
+        def __init__(self, cfunc, q, parents):
+            hx.ctree_visitor_t.__init__(self,
+                hx.CV_PARENTS if parents else hx.CV_FAST)
+
+            self.cfunc = cfunc
+            self.query = q
+            self.found = list()
+            return
+
+        def visit_expr(self, e):
+            """process cexpr_t elements"""
+            try:
+                if self.query(self.cfunc, e):
+                    self.found.append(query_result_t(self.cfunc, e))
+            except:
+                pass
+            return 0
+
+    if cfunc:
+        expfinder = expr_finder_t(cfunc, q, parents)
+        expfinder.apply_to_exprs(e, None)
+        return expfinder.found
+    return list()
+
+# ----------------------------------------------------------------------------
+def exec_query(q, ea_list, query_full, parents=False, flags=0):
+    """run query on list of addresses
+
+    convenience wrapper function around find_item()
+
+    arguments:
+    q:          lambda/function: f(cfunc_t, citem_t) returning a bool
+    ea_list:    iterable of addresses/functions to process
+    query_full: False -> find cexpr_t only (faster but doesn't find cinsn_t items)
+                True  -> find citem_t elements, which includes cexpr_t and cinsn_t
+
+    returns list of query_result_t objects
+    """
+
+    find_elem = find_item if query_full else find_expr
+    result = list()
+    for ea in ea_list:
+        result += find_elem(ea, q, parents=parents, flags=flags)
+    return result
+
+# ----------------------------------------------------------------------------
+def query_db(q, query_full=True, do_print=False):
+    """run query on idb
+    
+    arguments:
+    q:          lambda/function: f(cfunc_t, citem_t) returning a bool
+    query_full: False -> find cexpr_t only (default - faster but doesn't find cinsn_t items)
+                True  -> find citem_t elements, which includes cexpr_t and cinsn_t
+
+    returns list of query_result_t objects
+    """
+
+    return query(q, ea_list=idautils.Functions(), query_full=query_full, do_print=do_print)
+
+# ----------------------------------------------------------------------------
+def query(q, ea_list=None, query_full=True, do_print=False):
+    """run query on list of addresses
+
+    arguments:
+    q:          lambda/function: f(cfunc_t, citem_t) returning a bool
+    ea_list:    iterable of addresses/functions to process
+    query_full: False -> find cexpr_t only (default - faster but doesn't find cinsn_t items)
+                True  -> find citem_t elements, which includes cexpr_t and cinsn_t
+
+    returns list of query_result_t objects
+    """
+
+    if not ea_list:
+        ea_list = [ida_kernwin.get_screen_ea()]
+    r = list()
+    try:
+        r = exec_query(q, ea_list, query_full)
+        if do_print:
+            print("<query> done! %d unique hits." % len(r))
+            for e in r:
+                print(e)
+    except Exception as exc:
+        print("<query> error:", exc)
+    return r
+
+# ----------------------------------------------------------------------------
+class ic_t(ida_kernwin.Choose):
+    """Chooser for citem_t types
+
+    arguments:
+    q:          lambda/function: f(cfunc_t, citem_t) returning a bool
+                or list of query_result_t objects
+    ea_list:    iterable of addresses/functions to process
+    query_full: False -> find cexpr_t only (default - faster but doesn't find cinsn_t items)
+                True  -> find citem_t elements, which includes cexpr_t and cinsn_t
+    """
+    window_title = "Hexrays Toolbox"
+
+    def __init__(self,
+            q=None,
+            ea_list=None,
+            query_full=True,
+            flags=ida_kernwin.CH_RESTORE | ida_kernwin.CH_QFLT,
+            title=None,
+            width=None,
+            height=None,
+            embedded=False,
+            modal=False):
+        
+        _title = ""
+        i = 0
+        idx = ""
+        pfx = ""
+        exists = True
+        while exists:
+            idx = chr(ord('A')+i%26)
+            _title = "%s-%s%s" % (ic_t.window_title, pfx, idx)
+            if title:
+                _title += ": %s" % title
+            exists = (ida_kernwin.find_widget(_title) != None)
+            i += 1
+            pfx += "" if i % 26 else "A"
+
+        ida_kernwin.Choose.__init__(
+            self,
+            _title,
+            [ ["Function", 20 | ida_kernwin.CHCOL_FNAME],
+              ["Address", 10 | ida_kernwin.CHCOL_EA],
+              ["Output", 80 | ida_kernwin.CHCOL_PLAIN]],
+            flags = flags,
+            width = width,
+            height = height,
+            embedded = embedded)
+        
+        if ea_list is None:
+            ea_list =[ida_kernwin.get_screen_ea()]
+        if callable(q):
+            self.items = exec_query(q, ea_list, query_full)
+        elif isinstance(q, list):
+            self.items = q
+        else:
+            self.items = list()
+        self.Show()
+
+    def OnClose(self):
+        self.items = []
+
+    def OnSelectLine(self, n):
+        item_ea = self.items[n].ea
+        func_ea = self.items[n].entry
+        ea = func_ea if item_ea == BADADDR else item_ea
+        ida_kernwin.jumpto(ea)
+
+    def OnGetLine(self, n):
+        return self._make_choser_entry(n)
+
+    def OnGetSize(self):
+        return len(self.items)
+
+    def append(self, data):
+        if not isinstance(data, query_result_t):
+            return False
+        self.items.append(data)
+        self.Refresh()
+        return True
+
+    def set_data(self, data):
+        self.items = data
+        self.Refresh()
+
+    def get_data(self):
+        return self.items
+
+    def _make_choser_entry(self, n):
+        return ["%s" % idc.get_func_off_str(self.items[n].entry),
+                "%016x" % self.items[n].ea if __EA64__ else "%08x" % self.items[n].ea,
+                self.items[n].v]
---
+++ hxtb_shell.py
@@ -0,0 +1,294 @@
+import idautils
+import idaapi
+import hxtb
+import json
+import os
+from types import FunctionType
+
+# hxtb-shell - A graphical frontend for Hexrays Toolbox
+# URL: https://github.com/patois/HexraysToolbox
+
+__author__ = "@pat0is"
+SCRIPT_NAME = "hxtb-shell"
+
+try:
+    INSTANCE
+except:
+    INSTANCE = None
+
+def run_query(qf, ea_list, qs):
+    subtitle = qs.help
+    title = subtitle if len(subtitle) < 80 else "%s..." % subtitle[:77]
+    ch = hxtb.ic_t(title="Shell [%s]" % title)
+    mode = qs.ast_type==1
+
+    idaapi.show_wait_box("Processing")
+    try:
+        nfuncs = len(ea_list)
+        for j, ea in enumerate(ea_list):
+            if idaapi.user_cancelled():
+                break
+            idaapi.replace_wait_box("Processing function %d/%d" % (j+1, nfuncs))
+            r = list()
+            try:
+                r = hxtb.exec_query(qf, [ea], mode, parents=True, flags=idaapi.DECOMP_NO_WAIT)
+                for x in r:
+                    ch.append(x)
+            except Exception as e:
+                print("%s: %s" % (SCRIPT_NAME, e))
+    finally:
+        idaapi.hide_wait_box()
+    return ch
+
+def compile_code(s):
+    def wrap_lambda(lexpr):
+        return """class LambdaMatcher(hxtb.query_object_t):
+    def __init__(self):
+        pass
+    def init(self):
+        return True
+    def run(self, f, i):
+        return %s
+    def exit(self):
+        return
+return LambdaMatcher()""" % lexpr
+
+    qo = None
+    wl = s.query
+    if s.query_type == 1:
+        wl = wrap_lambda(wl)
+    try:
+        global ___hxtbshell_dynfunc_code___
+        q = "\t"+"\t".join(wl.splitlines(True))
+        foo_code = compile('def ___hxtbshell_dynfunc___():\n%s' % q, "hxtb-shell dyncode", "exec")
+        ___hxtbshell_dynfunc_code___ = FunctionType(foo_code.co_consts[0], globals(), "___hxtbshell_dynfunc___")
+        hack = eval("lambda: ___hxtbshell_dynfunc_code___()")
+        #instantiate
+        qo = hack()
+    except Exception as e:
+        print(e)
+    return qo
+
+def get_ealist(s, qo):
+    scope = s.scope
+    # scope = database
+    if scope == 0:
+        return list(idautils.Functions())
+    # scope = current function or xrefs to current item
+    elif scope == 1 or scope == 2:
+        screen_ea = idaapi.get_screen_ea()
+        # current function
+        if scope == 1:
+            return [screen_ea]
+        # xrefs to current item
+        else:
+            return get_func_xrefs(screen_ea)
+    # scope defined by query
+    elif scope == 3:
+        # and query_type is function
+        if s.query_type == 0:
+            return qo.get_scope()
+        elif s.query_type == 1:
+            return []
+    return []
+
+def get_func_xrefs(ea):
+    ea_list = []
+    for xea in idautils.XrefsTo(ea):
+        xf = idaapi.get_func(xea.frm)
+        if not xf:
+            print("[%s] warning: no function boundaries defined at %x" % (SCRIPT_NAME, xea.frm))
+        else:
+            ea_list.append(xf.start_ea)
+    # remove duplicates
+    ea_list = list(dict.fromkeys(ea_list))
+    return ea_list
+
+class QuerySettings():
+    def __init__(self, query="", query_qtype=1, ast_type=1, scope=1, help="new query"):
+        self.commit(query, query_qtype, ast_type, scope, help)
+
+    def commit(self, query, query_qtype, ast_type, scope, help):
+        self.version = 1.0
+        self.query = query
+        self.query_type = query_qtype
+        self.ast_type = ast_type
+        self.help = help
+        self.scope = scope
+        return
+
+    def save(self, filepath):
+        try:
+            with open(filepath, 'w') as fp:
+                json.dump(vars(self), fp, ensure_ascii=True)
+        except Exception as e:
+            return (False, e)
+        return (True, "")
+
+    def load(self, filepath):
+        try:
+            with open(filepath, 'r') as fp:
+                content = json.load(fp)
+                for k, v in content.items():
+                    setattr(self, k, v)
+        except Exception as e:
+            return (False, e)
+        return (True, "")
+
+
+class QueryForm(idaapi.Form):
+    def __init__(self):
+        form = r"""STARTITEM {id:mstr_query}
+BUTTON YES NONE
+BUTTON NO NONE
+BUTTON CANCEL NONE
+%s
+{FormChangeCb}
+<##~N~ew:{btn_new}><##~O~pen:{btn_load}><##~S~ave as...:{btn_save}>
+
+<:{str_help}>
+<Query (function or expression)\::{mstr_query}>
+
+<##Above code is a##Function:{rOptionFunction}>
+<Lambda expression (f=cfunc_t and i=citem_t):{rOptionExpression}>{rad_qtype}>
+
+<##Process AST elements##cot (faster):{rASTExpr}>
+<cit and cot:{rASTStmt}>{rad_ast_type}>
+
+<##Scope##Database:{rScopeIDB}>
+<Current function:{rScopeCurFunc}>
+<Xrefs to current item:{rScopeXrefItem}>
+<Defined by query:{rScopeQuery}>{rad_qscope}>     
+
+<##~R~un:{btn_runq}>
+""" % SCRIPT_NAME
+        self._qs = QuerySettings()
+        s = self._get_settings()
+        F = idaapi.Form
+        t = idaapi.textctrl_info_t()
+        controls = {"mstr_query": F.MultiLineTextControl(text=s.query,
+                        flags=t.TXTF_AUTOINDENT | t.TXTF_ACCEPTTABS | t.TXTF_FIXEDFONT | t.TXTF_MODIFIED,
+                        tabsize=2,
+                        width=90,
+                        swidth=90),
+                    "str_help": F.StringInput(swidth=90, value=s.help),
+                    "rad_qscope": F.RadGroupControl(
+                        ("rScopeIDB", "rScopeCurFunc", "rScopeXrefItem", "rScopeQuery"), value=s.scope),
+                    "rad_qtype": F.RadGroupControl(("rOptionFunction", "rOptionExpression"), value=s.query_type),
+                    "rad_ast_type": F.RadGroupControl(("rASTExpr", "rASTStmt"), value=s.ast_type),
+                    "btn_load": F.ButtonInput(self.OnButtonPress, code=0),
+                    "btn_save": F.ButtonInput(self.OnButtonPress, code=1),
+                    "btn_runq": F.ButtonInput(self.OnButtonPress, code=2),
+                    "btn_new": F.ButtonInput(self.OnButtonPress, code=3),
+                    'FormChangeCb': F.FormChangeCb(self.OnFormChange)}
+        F.__init__(self, form, controls)
+
+    def OnFormChange(self, fid):
+        if fid == -1: # form is intialized
+            self._apply_settings_to_ui(self._get_settings())
+        if fid == -5: # close form? (undocumented?)
+            self._commit_settings()
+        return 1
+
+    def _get_settings(self):
+        return self._qs
+
+    def _apply_settings_to_ui(self, settings):
+        tc = self.GetControlValue(self.mstr_query)
+        tc.text = settings.query
+        self.SetControlValue(self.mstr_query, tc)
+        self.SetControlValue(self.rad_ast_type, settings.ast_type)
+        self.SetControlValue(self.rad_qtype, settings.query_type)
+        self.SetControlValue(self.rad_qscope, settings.scope)
+        self.SetControlValue(self.str_help, settings.help)
+        return 
+
+    def _commit_settings(self):
+        settings = self._get_settings()
+        settings.commit(
+            self.GetControlValue(self.mstr_query).text,
+            self.GetControlValue(self.rad_qtype),
+            self.GetControlValue(self.rad_ast_type),
+            self.GetControlValue(self.rad_qscope),
+            self.GetControlValue(self.str_help))
+        return
+
+    def _handle_btn_load_tbq_file(self, filepath):
+        settings = self._get_settings()
+        success, e = settings.load(filepath)
+        if success:
+            if settings.version < 1.0:
+                idaapi.warning("Version not supported")
+                return
+            self._apply_settings_to_ui(settings)
+        else:
+            idaapi.warning("Could not load file.\n\n%s" % e)
+            return
+        print("[%s] loaded from \"%s\"" % (SCRIPT_NAME, filepath))
+        return
+
+    def _handle_btn_save_tbq_file(self, filepath):
+        if os.path.exists(filepath):
+            if idaapi.ASKBTN_YES != idaapi.ask_yn(idaapi.ASKBTN_NO, "File exists!\n\nOverwerite %s?" % filepath):
+                return
+        self._commit_settings()
+        success, e = self._get_settings().save(filepath)
+        if success:
+            print("[%s] saved to \"%s\"" % (SCRIPT_NAME, filepath))
+        else:
+            idaapi.warning("Could not save file.\n\n%s" % e)
+        return
+
+    def _handle_btn_run_query(self):
+        self._commit_settings()
+        settings = self._get_settings()
+        qo = compile_code(settings)
+        if qo:
+            # call init() and check whether it is ok to run this query
+            if qo.init():
+                ea_list = get_ealist(settings, qo)
+                if not len(ea_list):
+                    idaapi.warning("%s: empty scope!" % SCRIPT_NAME)
+                else:
+                    """run query on 'ea_list'
+                    on a side note: passing an object's method as an argument to hxtb
+                    is probably(?) a bad idea and I surely do not know how it works
+                    under the hood but it seems to work for the time being."""
+                    run_query(qo.run, ea_list, settings)
+                # call cleanup/exit function
+                qo.exit()
+        return
+
+    def _handle_btn_new(self):
+        # apply empty settings
+        self._apply_settings_to_ui(QuerySettings())
+        return
+
+    def OnButtonPress(self, code=0):
+        if code == 0:
+            path = idaapi.ask_file(False, "*.tbq", "Load hxtb query from file...")
+            if path:
+                self._handle_btn_load_tbq_file(path)
+        elif code == 1:
+            path = idaapi.ask_file(True, "*.tbq", "Save hxtb query to file...")
+            if path:
+                self._handle_btn_save_tbq_file(path)
+        elif code == 2:
+            self._handle_btn_run_query()
+        elif code == 3:
+            self._handle_btn_new()
+        else:    
+            idaapi.warning("wtf?")
+
+    @staticmethod
+    def open():
+        global INSTANCE
+        if INSTANCE is None:
+            form = QueryForm()
+            form.modal = False
+            form, _ = form.Compile()
+            INSTANCE = form
+        return INSTANCE.Open()
+
+if __name__ == "__main__":
+    QueryForm.open()
